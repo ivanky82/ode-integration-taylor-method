aea16c9b5e82c54a32c90adfc93e3be3b9c9b8fa
Ivanky Saputra <ivanky82@gmail.com>
2017-12-12 14:09:16 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from mytaylor import *
+import matplotlib.pyplot as plt
+import numpy as np
+import math as m
+
+def fun(y):
+    z = subtractpoli(y[0],multiplypoli(y[0],y[0]))
+    return [z]
+
+y_init = [0.5]
+t_init = 0
+h = 0.1
+tStop = 1.0
+T, Y = integrate(fun,y_init,t_init,tStop,h)
+
+#real solution computation
+real_sol = []
+for t in T:
+	temp = m.exp(t)/(m.exp(t)+1.0)
+	real_sol.append(temp)
+print Y
+print real_sol
+#plotting the error versus time
+real_sol = np.array(real_sol)
+error = Y[:,0]-real_sol
+#plt.plot(T[:],error[:])
+#plt.show()
